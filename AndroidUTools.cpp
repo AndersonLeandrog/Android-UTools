2cab8baf8f3245ccdf6a335e982d638fc061df66
Anderson Leandro <65420034+AndersonLeandrog@users.noreply.github.com>
2021-07-12 18:33:54 +0100

    Update AndroidUTools.cpp

--- AndroidUTools.cpp
+++ AndroidUTools.cpp
@@ -10,24 +10,26 @@ using namespace std;
 
 int main() 
 {
-	//exibe o titulo do programa (nao esqueÃ§a de dar os creditos ao desenvolvedor original do projeto)
-	system("title AndroidUserTools by OCOD ~ ver 1.3.4 (beta)");
+	//titulo do programa
+	system("title AndroidUserTools by OCOD ~ ver 1.3.4 (stable)");
 
-	//cria a variavel proc de 'processo' e key de 'chave'
+	//cria uma variavel um inteira
 	int proc;
-	char key[30];
 	
-	/*exibe uma mensagem perguntando sobre o tipo de proceso a ser realizado para o usuario e grava a
-	resposta na variavel proc*/
+	//exibe uma mensagem ao usuario e grava a resposta na variavel
 	cout << "\n Select a process to start\n\n  [1] TWRP Install\n\n  [2] Bootloader Unlock (Motorola)\n  [3] Remove bootloader logo\n\n  [4] Magisk/Tutorial\n\n  [5] Install APK/ADB\n  [6] Unistall APK/ADB\n\n > ";
 	cin >> proc;
 	
 	system("cls");
 	
+	//executa um processo dependendo da resposta dentro de um bloco switch
 	switch(proc)
 	{
-		case 1: //twrp 
-			//exibe uma mensagem de instruÃ§ao para a instalaÃ§ao do TWRP e aguarda ate que uma tecla seja pressionada
+		case 1: //se proc for igual a 1
+			
+			char path[100]; //cria uma variavel com o nome path que suporte 100 caracteres
+			
+			//exibe uma mensagem orientando o usuario
 			cout << "\n INSTRUCTION:  download  the twrp  image  to your device," << endl;
 			cout << " connect your device to the PC and  press  [ENTER],  on  the" << endl;
 			cout << " next screen, move the twrp image file to the command prompt" << endl;
@@ -41,64 +43,44 @@ int main()
 			
 			system("cls");
 			
-			//cria uma variavel com o nome path 
-			char path[100];
-			
-			//>> aqui!
 			cout << "\n REQUIREMENTS FOR INSTALATION" << endl;
 			cout << "\n * ADB and Fastboot drivers, visit: https://adb.clockworkmod.com/" << endl;
 			cout << " * Drivers of your mobile device (look for the driver for your device model)" << endl;
 			cout << " * Unlocked bootloader (see the video on how to unlock on my youtube channel)" << endl;
 			cout << "   [https://www.youtube.com/channel/UCqnVsLksN6n7XjpDP-urM5Q]\n\n\n\n";
-			
-			
-			cout << "                   DRAG THE IMG FILE HERE,                \n";
-			cout << "               AND PRESS [ENTER] TO CONTINUE!             \n";
-			cout << "                                                          \n";
-			cout << "               0 0   0 00000  0000 0 0    0000            \n";
-			cout << "               0 0 0 0 0      0    0 0    0               \n";
-			cout << "               0 0   0 0  00  000  0 0    0000            \n";
-			cout << "               0 0   0 0   0  0    0 0    0               \n";
-			cout << "             0 0 0   0 00000  0    0 0000 0000            \n";
+				
+			cout << "   DRAG THE IMG FILE HERE,         \n";
+			cout << "   AND PRESS [ENTER] TO CONTINUE!  \n";
+			cout << "                                   \n";
+			cout << "   0 0   0 00000  0000 0 0    0000 \n";
+			cout << "   0 0 0 0 0      0    0 0    0    \n";
+			cout << "   0 0   0 0  00  000  0 0    0000 \n";
+			cout << "   0 0   0 0   0  0    0 0    0    \n";
+			cout << " 0 0 0   0 00000  0    0 0000 0000 \n";
 			cout << "\n\n > ";
-			cin >> path;
+			cin >> path; //grava o caminho do arquivo em path
 			
-			FILE * pFile; //cria um ponteiro para o arquivo
-			pFile = fopen ("grab.txt","wt"); //cria e abre o arquivo em modo de gravaï¿½ao
-			fprintf (pFile, "move %s",path); //grava uma string seguida de uma variavel
-			fclose (pFile); //fecha o arquivo
+			/*cria e abre um arquivo do tipo .txt em modo de gravacao e em seguida grava uma
+			string seguida do codigo dentro da variavel path*/
+			FILE * file_path; 
+			file_path = fopen ("path.txt","wt"); 
+			fprintf (file_path, "move %s",path); 
+			fclose (file_path); 
 		   	
-		   	system("ren grab.txt grab.bat"); //renomeia o arquivo criado para .bat
-		   	system("grab.bat"); //executa o arquivo .bat
-		   	system("del grab.bat"); //deleta o arquivo .bat depois da sua execuï¿½ao
+		   	//operacoes com o arquivo criado
+		   	system("ren path.txt path.bat"); 
+		   	system("path.bat"); 
+		   	system("del path.bat"); 
 			
 			system("cls");
 			
-			system("title AndroidUserTools by OCOD _ recognizing the device...");
-			system("fastboot devices"); //reconhece qualquer aparelho conectado
-			Sleep(2000);
+			//operacoes com o arquivo de imagem
+			system("ren *.img twrp.img"); 
+			system("fastboot flash recovery twrp.img"); 
+			system("fastboot boot twrp.img"); 
 			
 			system("cls");
 			
-			system("title AndroidUserTools by OCOD _ renaming file...");
-			system("ren *.img twrp.img"); //renomeia todos os arquivos de imagem para twrp.img
-			Sleep(1000);
-			
-			system("cls");
-			
-			system("title AndroidUserTools by OCOD _ flashing the device...");
-			system("fastboot flash recovery twrp.img"); //flesha o twrp no celular
-			Sleep(3000);
-			
-			system("cls");
-			
-			system("title AndroidUserTools by OCOD _ restarting the device...");
-			system("fastboot boot twrp.img"); //reinicia o aparelho no modo de recuperaï¿½ao
-			Sleep(1000);
-			
-			system("cls");
-			
-			system("title AndroidUserTools by OCOD _ done!");
 			cout << "\n if everything worked out your device will restart in TWRP recovery mode." << endl;
 			cout << "\n press [enter] to exit..." << endl;
 			getch();
@@ -106,7 +88,10 @@ int main()
 			exit(0);
 		break;
 			
-		case 2: //Bootloader
+		case 2: //se proc for igual a 2
+			
+			char key[30]; //cria uma variavel char que armazena ate 30 caracteres
+			
 			//exibe uma mensagem ao usuario
 			cout << "\n connect your device in recovery mode and press [ENTER]...";
 			getch();
@@ -116,8 +101,7 @@ int main()
 			//executa o comando para obter o codigo de desbloqueio do aparelho
 			system("fastboot oem get_unlock_data");
 	
-			/*exibe um pequeno tutorial de como obter o codigo de desbloqueio e aguarda ate que
-			uma tecla seja pressionada para continuar*/
+			//exibe uma mensagem ao usuario
 			cout << "\n\n\n Copy all the code above and press [ENTER] <-";
 			cout << "\n\n  > right click on the code and select (mark),";
 			cout << "\n    select the entire code and click again with";
@@ -129,73 +113,72 @@ int main()
 			cout << "\n                      ^                  ^                  ^\n\n > ";
 			getch();
 			
-			/*limpa a tela e abre o arquivo web unlock.html para que o usuario insira o codigo
-			obtido pelo prompt de comando*/
 			system("cls");
+			
+			//abre o arquivo unlock.html 
 			system("unlock.htm");
 	
-			/*exige o codigo de desbloqueio enviado ao e-mail para o usuario
-			e grava o codigo na variavel do tipo char key*/
+			//exige o codigo de desbloqueio enviado ao e-mail, key armazena o codigo de ate 30 caracteres
 			cout << "\n Put Your Unlock code here and press [ENTER] > ";
 			cin >> key;
 			
 			/*cria e abre um arquivo do tipo .txt em modo de gravacao e em seguida grava uma
 			string seguida do codigo dentro da variavel key*/
-			FILE * arq_key; 
-			arq_key = fopen ("key.txt","wt"); 
-			fprintf (arq_key, "fastboot oem unlock %s",key); 
-			fclose (arq_key); 
+			FILE * file_key; 
+			file_key = fopen ("key.txt","wt"); 
+			fprintf (file_key, "fastboot oem unlock %s",key); 
+			fclose (file_key); 
 			
-			/*renomeia o arquivo key.txt criado para key.bat e executa esse arquivo em seguida,
-			apos sua execuÃ§ao o arquivo e removido da pasta raiz*/
+			//operecoes com o arquivo criado
 			system("ren key.txt key.bat");
 	   		system("key.bat"); 
 	   		system("del key.bat"); 
 	   		system("cls");
 	   		
-			/*exibe a mensagem de sucesso, bootloader foi desbloqueado! e aguarda ate que uma tecla
-			seja pressionada para continuar*/
+			//exibe uma mensagem de sucesso
 	   		cout << "\n Bootloader has been unlocked, press [ENTER] to exit...";
 	   		getch();
 	   		
-	   		exit(0); //finaliza o programa
+	   		exit(0);
 		break;
 		
-		case 3: //bootloader logo
-			char bin[10];
+		case 3: //se proc for igual a 3
+		
+			//cria uma variavel do tipo char que suporta ate 120 caracteres
+			char bin[120];
 			
+			//exibe uma cadeia de mensagens
 			cout << "\n Connect your device in fastboot mode and press [ENTER]...";
 			getch();
 			
 			system("cls");
 			
 			cout << "\nCheck if your device has been recognized, if so press [ENTER] again...\n\n";
-			system("fastboot devices");
+			system("fastboot devices"); //verfica se existe algum aparelho conectado
 			getch();
 			
 			system("cls");
 			
 			cout << "\n You will need the logo file for your cell phone model\n in .bin format, drag the file to the console below and press [ENTER]\n\n\n";
-			cout << "                   DRAG THE BIN FILE HERE,                \n";
-			cout << "               AND PRESS [ENTER] TO CONTINUE!             \n";
-			cout << "                                                          \n";
-			cout << "              0000  0 0   0  0000 0 0    0000             \n";
-			cout << "              0   0 0 00  0  0    0 0    0                \n";
-			cout << "              0000  0 0 0 0  000  0 0    0000             \n";
-			cout << "              0   0 0 0  00  0    0 0    0                \n";
-			cout << "            0 0000  0 0   0  0    0 0000 0000             \n";
+			cout << "   DRAG THE BIN FILE HERE,          \n";
+			cout << "   AND PRESS [ENTER] TO CONTINUE!   \n";
+			cout << "                                    \n";
+			cout << "   0000  0 0   0  0000 0 0    0000  \n";
+			cout << "   0   0 0 00  0  0    0 0    0     \n";
+			cout << "   0000  0 0 0 0  000  0 0    0000  \n";
+			cout << "   0   0 0 0  00  0    0 0    0     \n";
+			cout << " 0 0000  0 0   0  0    0 0000 0000  \n";
 			cout << "\n\n > ";	
-			cin >> bin;
+			cin >> bin; //grava o caminho do arquivo
 			
 			/*cria e abre um arquivo do tipo .txt em modo de gravacao e em seguida grava uma
 			string seguida do codigo dentro da variavel bin*/
-			FILE * arq_bin; 
-			arq_bin = fopen ("bin.txt","wt"); 
-			fprintf (arq_bin, "fastboot flash logo %s",bin); 
-			fclose (arq_bin); 
+			FILE * file_bin; 
+			file_bin = fopen ("bin.txt","wt"); 
+			fprintf (file_bin, "fastboot flash logo %s",bin); 
+			fclose (file_bin); 
 			
-			/*renomeia o arquivo bin.txt criado para bin.bat e executa esse arquivo em seguida,
-			apos sua execuÃ§ao o arquivo e removido da pasta raiz*/
+			//operacoes com o arquivo criado
 			system("ren bin.txt bin.bat");
 	   		system("bin.bat"); 
 	   		system("del bin.bat"); 
@@ -208,7 +191,9 @@ int main()
 			exit(0);
 			break;
 			
-		case 4: //Magisk
+		case 4: //se proc for igual a 4
+		
+			//exibe uma mensagem/tutorial de instalacao do magisk
 			cout << "\n Magisk/Tutorial";
 			cout << "\n  How to install Magisk";
 			cout << "\n\n 1. Go to Magisk page on GitHub: https://github.com/topjohnwu/Magisk";
@@ -222,45 +207,50 @@ int main()
 			exit(0);	
 			break;
 			
-		case 5:	//apk install
+		case 5:	//se proc for igual a 5
+			
+			//usa goto, recarrega o codigo a partir daqui
 			reload:
 				
+			//cria duas variaveis do tipo char, com uma suportando ate 500 caracteres
 			char apk[500];
 			char answer;
 			
+			//exibe uma mensagem
 			cout << "\n Enable usb debugging on your device,";
 			cout << "\n\n  Go to your device settings, enable developer mode\n  and within that option enable usb debug mode!\n\n";
-			cout << "                   DRAG THE APK FILE HERE,                \n";
-			cout << "               AND PRESS [ENTER] TO CONTINUE!             \n";
-			cout << "                                                          \n";
-			cout << "               0   0000  0  0  0000 0 0    0000          \n";
-			cout << "              0 0  0   0 0 0   0    0 0    0             \n";
-			cout << "             00000 0000  00    000  0 0    0000          \n";
-			cout << "             0   0 0     0 0   0    0 0    0             \n";
-			cout << "           0 0   0 0     0  0  0    0 0000 0000          \n";
+			cout << "   DRAG THE APK FILE HERE,             \n";
+			cout << "   AND PRESS [ENTER] TO CONTINUE!      \n";
+			cout << "                                       \n";
+			cout << "     0   0000  0  0  0000 0 0    0000  \n";
+			cout << "    0 0  0   0 0 0   0    0 0    0     \n";
+			cout << "   00000 0000  00    000  0 0    0000  \n";
+			cout << "   0   0 0     0 0   0    0 0    0     \n";
+			cout << " 0 0   0 0     0  0  0    0 0000 0000  \n";
 			cout << "\n\n > ";	
-			cin >> apk;
+			cin >> apk; //grava o caminho do arquivo na variavel apk
 			
 			/*cria e abre um arquivo do tipo .txt em modo de gravacao e em seguida grava uma
 			string seguida do codigo dentro da variavel apk*/
-			FILE * arq_apk; 
-			arq_apk = fopen ("apk.txt","wt"); 
-			fprintf (arq_apk, "adb install -r %s",apk); 
-			fclose (arq_apk); 
+			FILE * file_apk; 
+			file_apk = fopen ("apk.txt","wt"); 
+			fprintf (file_apk, "adb install -r %s",apk); 
+			fclose (file_apk); 
 			
-			/*renomeia o arquivo apk.txt criado para key.bat e executa esse arquivo em seguida,
-			apos sua execuÃ§ao o arquivo e removido da pasta raiz*/
+			//operacoes com o arquivo criado
 			system("ren apk.txt apk.bat");
 	   		system("apk.bat"); 
 	   		system("del apk.bat"); 
+	   		
 	   		system("cls");
 	   		
-			/*exibe a mensagem de sucesso, aplicativo foi instalado com sucesso!*/
+			//exibe uma mensagem de sucesso e pergunta se o usuario deseja executar o programa mais uma vez
 	   		cout << "\n " << apk << " - has been successfully installed!";
 	   		cout << "\n\n Install another apk? [y]//[n]";
 	   		cout << "\n\n > ";
 	   		cin >> answer;
 	   		
+	   		//se sim ele carrega reload, caso nao ele sai do programa
 	   		if (answer == 'y' or answer == 'Y')
 	   		{
 	   			system("cls");
@@ -270,13 +260,16 @@ int main()
 			}
 			break;
 			
-		case 6: //apk uninstall
+		case 6: //se proc for igual a 6
+		
+			//exibe uma mensagem
 			cout << "\n Enable usb debugging on your device,";
 			cout << "\n\n  Go to your device settings, enable developer mode\n  and within that option enable usb debug mode!\n";	
 			cout << "\n\n  type the command followed by the package name as in\n  the example below and press [ENTER]\n";
 			cout << "\n  exemple: pm uninstall -k --user 0 com.yourpackage.name";	
 			cout << "\n           ---------command-------- -----packagename----\n\n\n";
 			
+			//executa um comando
 			system("adb shell");
 	   		getch();
 			
