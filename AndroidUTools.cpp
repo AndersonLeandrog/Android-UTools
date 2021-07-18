44bc304a482701bcabc5112cc74b63125204f568
Anderson Leandro <65420034+AndersonLeandrog@users.noreply.github.com>
2021-07-18 21:27:23 +0100

    Update AndroidUTools.cpp

--- AndroidUTools.cpp
+++ AndroidUTools.cpp
@@ -15,76 +15,77 @@ void apkInstall();
 void apkUninstall();
 
 int main() {
+	int processo;
 
-	//titulo do programa
+	//Exibe um título e modifica a cor do texto
 	system("title @OCOD ~ AndroidUserTools_v1.3.6 (stable)");
-
 	textcolor(15);
 
-	//cria uma variavel um inteira
-	int proc;
-
-	//exibe uma mensagem ao usuario e grava a resposta na variavel
+	//Exibe uma mensagem ao usuário e grava o valor na variável 'processo'
 	cout << "\n Select a process to start\n\n  [1] TWRP Install\n\n  [2] Bootloader Unlock (Motorola)\n  [3] Remove bootloader logo\n\n  [4] Magisk/Tutorial\n\n  [5] Install APK/ADB\n  [6] Unistall APK/ADB\n\n > ";
-	cin >> proc;
+	cin >> processo;
 
 	clrscr();
 
-	//executa um processo dependendo da resposta dentro de um bloco switch
-	switch(proc) {
-		case 1:
-			twrp();
+	switch(processo) 
+	{
+		case 1: //Caso o processo seja igual a 1
+			twrp(); //Executa a função twrp();
 			break;
 
-		case 2:
-			bootloader();
+		case 2: //Caso o processo seja igual a 2
+			bootloader(); //Executa a função bootloader()
 			break;
 
-		case 3:
-			bootloaderLogo();
+		case 3: //Caso o processo seja igual a 3
+			bootloaderLogo(); //Executa a função bootloaderlogo()
 			break;
 
-		case 4:
-			magisk();
+		case 4: //Caso o processo seja igual a 4
+			magisk(); //Executa a função magisk();
 			break;
 
-		case 5:
-			apkInstall();
+		case 5: //Caso o processo seja igual a 5
+			apkInstall(); //Executa a função apkInstall()
 			break;
 
-		case 6:
-			apkUninstall();
+		case 6: //Caso o processo seja igual a 6
+			apkUninstall(); //Executa a função apkUninstall()
 			break;
 	}
 }
 
-void twrp() {
 
-	char path[100]; //cria uma variavel com o nome path que suporte 100 caracteres
 
-	textcolor(15); //exibe uma mensagem com o texto opaco em cinza
+/*
+	FUNÇÃO TWRP
+*/
+
+void twrp() { 
+	char path[100];
+	textcolor(15); //Branco brilhante
+
 	cout << "\n INSTRUCTION:  download  the twrp  image  to your device," << endl;
 	cout << " connect your device to the PC and  press  [ENTER],  on  the" << endl;
 	cout << " next screen, move the twrp image file to the command prompt" << endl;
 	cout << " press [ENTER] again and  wait  for  the  image  to install." << endl;
+    textcolor(6); //Cinza (Opaco)
 
-    textcolor(6); //cinza
 	cout << "\n ! it is not necessary to rename the file to (twrp.img)," << endl;
 	cout << "     the program will do this during the installation process..." << endl;
-
 	cout << "\n\n connect your device to PC in recovery mode and press [ENTER]..." << endl;
+	textcolor(15); //Branco brilhante
 	getch();
 
 	clrscr();
 
-    textcolor(15); //cinza
 	cout << "\n REQUIREMENTS FOR INSTALATION" << endl;
 	cout << "\n * ADB and Fastboot drivers, visit: https://adb.clockworkmod.com/" << endl;
 	cout << " * Drivers of your mobile device (look for the driver for your device model)" << endl;
 	cout << " * Unlocked bootloader (see the video on how to unlock on my youtube channel)" << endl;
 	cout << "   [https://www.youtube.com/channel/UCqnVsLksN6n7XjpDP-urM5Q]\n\n\n\n";
 
-    textcolor(6); //verde
+    textcolor(6); //Verde
 	cout << "   DRAG THE IMG FILE HERE,         \n";
 	cout << "   AND PRESS [ENTER] TO CONTINUE!  \n";
 	cout << "                                   \n";
@@ -94,52 +95,67 @@ void twrp() {
 	cout << "   0 0   0 0   0  0    0 0    0    \n";
 	cout << " 0 0 0   0 00000  0    0 0000 0000 \n";
 	cout << "\n\n > ";
-	cin >> path; //grava o caminho do arquivo em path
+	cin >> path; //grava o caminho do arquivo na variável path
 
-	/*cria e abre um arquivo do tipo .txt em modo de gravacao e em seguida grava uma
-	string seguida do codigo dentro da variavel path*/
+	/*
+	Cria e abre um arquivo do tipo .txt em modo de gravação e em seguida grava uma
+	string seguida do código dentro da variável path
+	*/
+
 	FILE * file_path;
 	file_path = fopen ("path.txt","wt");
 	fprintf (file_path, "move %s",path);
 	fclose (file_path);
 
-	//operacoes com o arquivo criado
+	/*
+	Renomeia o arquivo criado de txt p/ bat, executa o bat e em seguida remove o arquivo
+	após a sua execução.
+	*/
+
 	system("ren path.txt path.bat");
 	system("path.bat");
 	system("del path.bat");
 
 	clrscr();
 
-	//operacoes com o arquivo de imagem
+	/*
+	Renomeia o arquivo de imagem do usuário para twrp.img e em seguida realiza o processo
+	de instalação do recovery
+
+	O primeiro comando flasheia o twrp renomeado no aparelho.
+	O segundo reinicia o aparelho após o processo.
+	*/
+
 	system("ren *.img twrp.img");
 	system("fastboot flash recovery twrp.img");
 	system("fastboot boot twrp.img");
+	textcolor(15); //branco brilhante
 
 	clrscr();
 
-    textcolor(15); //branco
 	cout << "\n if everything worked out your device will restart in TWRP recovery mode." << endl;
 	cout << "\n press [enter] to exit..." << endl;
-	getch();
+	getch(); //Se o botão enter for pressionado o programa será finalizado.
 
 	exit(0);
 }
 
+/*
+	FUNÇÃO BOOTLOADER
+*/
+
 void bootloader() {
-
-	char key[30]; //cria uma variavel char que armazena ate 30 caracteres
-
-	//exibe uma mensagem ao usuario
+	char key[30];
 	textcolor(6);
+
 	cout << "\n connect your device in recovery mode and press [ENTER]...";
 	getch();
 
 	clrscr();
 
-	//executa o comando para obter o codigo de desbloqueio do aparelho
+	//Executa o comando para obter o código de desbloqueio do aparelho
 	system("fastboot oem get_unlock_data");
 
-	//exibe uma mensagem ao usuario
 	cout << "\n\n\n Copy all the code above and press [ENTER] <-";
 	cout << "\n\n  > right click on the code and select (mark),";
 	cout << "\n    select the entire code and click again with";
@@ -155,7 +171,9 @@ void bootloader() {
 
 	//abre o arquivo unlock.html
 	system("unlock.htm");
-
+    
+	//AQUI ----------------------------------------------------------------------------------------
+	
 	//exige o codigo de desbloqueio enviado ao e-mail, key armazena o codigo de ate 30 caracteres
 	textcolor(15);
 	cout << "\n Put Your Unlock code here and press [ENTER] > ";
