0aa22afda94f89b08842ab81a9ae46a065b23340
Anderson Leandro <65420034+AndersonLeandrog@users.noreply.github.com>
2021-08-02 18:32:47 +0100

    Update adb.h

--- SourceFiles/adb.h
+++ SourceFiles/adb.h
@@ -4,21 +4,20 @@
 void adbPull()
 {
     reload:
-
-    std::cout << "\n All your moved files go to the program root\n folder called 'files'\n\n";
-    std::cout << " Copy files from device to PC\n";
-    std::cout << " enter the path of the file that will\n be moved from your phone to your computer.\n\n\n";
+    
+    cout << "\n All your moved files go to the program root\n folder called 'files'\n\n";
+    cout << " Copy files from device to PC\n";
+    cout << " enter the path of the file that will\n be moved from your phone to your computer.\n\n\n";
 
     textcolor(10);
-
-    std::cout << "           * from your device"; textcolor(5);
-    std::cout << "                      * to PC\n"; textcolor(15);
-    std::cout << "  Exemple: "; textcolor(10);
-    std::cout << "sdcard/android/data/your-file.extension"; textcolor(5);
-    std::cout << " /files"; textcolor(15);
-    std::cout << "\n\n digit '/r' to return";
-    std::cout << "\n\n > ";
-    std::cin >> archives;
+    cout << "           * from your device"; textcolor(4);
+    cout << "                      * to PC\n"; textcolor(15);
+    cout << "  Exemple: "; textcolor(10);
+    cout << "sdcard/android/data/your-file.extension"; textcolor(4);
+    cout << " /files"; textcolor(15);
+    cout << "\n\n digit '/r' to return";
+    cout << "\n\n > ";
+    cin >> archives;
 
     clrscr();
 
@@ -46,20 +45,19 @@ void adbPush()
 {
     reload:
 
-    std::cout << "\n All your moved files go to the 'usr.files'\n folder in your device's internal directory!\n";
-    std::cout << "\n Copy files from PC to Device";
-    std::cout << "\n enter the path of the file that will\n be moved from your PC to your phone\n or drag the file into the program.\n\n\n";
+    cout << "\n All your moved files go to the 'usr.files'\n folder in your device's internal directory!\n";
+    cout << "\n Copy files from PC to Device";
+    cout << "\n enter the path of the file that will\n be moved from your PC to your phone\n or drag the file into the program.\n\n\n";
 
     textcolor(10);
-
-    std::cout << "           * from your PC"; textcolor(5);
-    std::cout << "                  * to Device\n"; textcolor(15);
-    std::cout << "  Exemple: "; textcolor(10);
-    std::cout << "user/desktop/yourfile.extension"; textcolor(5);
-    std::cout << " sdcard/usr.files"; textcolor(15);
-    std::cout << "\n\n digit '/r' to return";
-    std::cout << "\n\n > ";
-    std::cin >> archives;
+    cout << "           * from your PC"; textcolor(4);
+    cout << "                  * to Device\n"; textcolor(15);
+    cout << "  Exemple: "; textcolor(10);
+    cout << "user/desktop/yourfile.extension"; textcolor(4);
+    cout << " sdcard/usr.files"; textcolor(15);
+    cout << "\n\n digit '/r' to return";
+    cout << "\n\n > ";
+    cin >> archives;
 
     clrscr();
 
@@ -70,7 +68,7 @@ void adbPush()
 
     //generates a txt file with instructions to move the created folder to the device in sdcard/'usr.files'
     //and then moves the files from the address stored in the archives variable to that folder moved to the device.
-    std::ofstream adbPushD;
+    ofstream adbPushD;
     adbPushD.open("path.txt");
     adbPushD << "adb push usr.files" << " " << "sdcard/" << "\n" << "adb push" << " " << archives << " " << "sdcard/usr.files";
     adbPushD.close();
