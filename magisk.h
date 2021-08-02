9fe433ffc7c9ab8c86458f3a5905c568b0b9cb7d
Anderson Leandro <65420034+AndersonLeandrog@users.noreply.github.com>
2021-08-02 16:13:46 +0100

    Add files via upload

---
+++ magisk.h
@@ -0,0 +1,25 @@
+#ifndef MAGISK_H_INCLUDED
+#define MAGISK_H_INCLUDED
+
+void magisk()
+{
+	textcolor(15);
+
+	cout << "\n Magisk/Tutorial";
+	cout << "\n  How to install Magisk";
+	cout << "\n\n 1. Go to Magisk page on GitHub: https://github.com/topjohnwu/Magisk";
+	cout << "\n\n 2. Download the Magisk zip file and move the zip from your PC\n    to your device's SD card (recommended)";
+	cout << "\n\n 3. Open TWRP on your device, click 'install'\n    and look for the magisk zip file";
+	cout << "\n\n 4. Select the zip and flash file...";
+	cout << "\n\n 5. After flashing the file, boot the system and install\n    the latest version of margisk.apk";
+	cout << "\n\n\n done! now you can enjoy the magic!";
+	cout << "\n\n digit '/r' to return \n\n > ";
+	cin >> archives; //grava o caminho do arquivo na variável path
+
+    clrscr();
+
+    if(archives == "/r") { system("AndroidUserTools.exe"); }
+	exit(0);
+}
+
+#endif // MAGISK_H_INCLUDED
